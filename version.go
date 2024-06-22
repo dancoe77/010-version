b178f526d627513b76fd19bd858a85de1ee10528
Dan S <dan@shoeman-tech.com>
2024-06-21 18:41:06 -0700

    first commit

---
+++ version.go
@@ -0,0 +1,11 @@
+package version
+
+var Linux string = "I am Linux"
+
+var Windows string = "I am Windows"
+
+var Fedora string = "I am Fedora"
+
+const Versionlinux int = 40
+
+const Versionwindows int = 40
